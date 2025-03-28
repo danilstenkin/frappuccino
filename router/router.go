083b34da9f6ed5ef63d5d7e74e3c33f94e81e3f6
dstenkin <danilstenkin57@gmail.com>
2025-03-28 15:18:42 +0500

    feat: create CRUD handlers for menu

--- router/router.go
+++ router/router.go
@@ -1,10 +1,33 @@
 package router
 
 import (
+	"frappuccino/handlers"
 	"net/http"
 )
 
 func SetupRouter() {
+	http.HandleFunc("/menu", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			handlers.CreateMenuItemHandler(w, r)
+		} else if r.Method == http.MethodGet {
+			handlers.GetMenuItemsHandler(w, r)
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.HandleFunc("/menu/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodDelete {
+			handlers.DeleteMenuItemHandler(w, r)
+		} else if r.Method == http.MethodGet {
+			handlers.GetMenuItemsIDHandler(w, r)
+		} else if r.Method == http.MethodPut {
+			handlers.UpdateMenuItemHandler(w, r)
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+	})
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic("Failed to start server: " + err.Error())
