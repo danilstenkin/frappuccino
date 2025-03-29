e1977312193e6bab17af509f242f14fb35874fc2
dstenkin <danilstenkin57@gmail.com>
2025-03-29 19:46:14 +0500

    feat: DELETE customer

--- handlers/customers_handler.go
+++ handlers/customers_handler.go
@@ -2,11 +2,13 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"frappuccino/db"
 	"frappuccino/models"
 	"frappuccino/repositories"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func CreateCustomersHandlers(w http.ResponseWriter, r *http.Request) {
@@ -53,4 +55,27 @@ func CreateCustomersHandlers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(response)
+}
+
+// DELETE -------------------------------------------------------------------------------------------------------
+
+func DeleteCustomersHandler(w http.ResponseWriter, r *http.Request) {
+	const logPrefix = "[DeleteMenuItemHandler]"
+
+	id := strings.TrimPrefix(r.URL.Path, "/customers/")
+
+	if id == "" {
+		log.Printf("%s Missing customers ID in URL", logPrefix)
+		http.Error(w, "ID not found", http.StatusBadRequest)
+		return
+	}
+
+	err := repositories.DeleteCustomer(id)
+	if err != nil {
+		log.Printf("%s Failed to delete customers: %v", logPrefix, err)
+		http.Error(w, fmt.Sprintf("Failed to delete customers: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
